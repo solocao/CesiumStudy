da6e976af0dd8726d08a98bd4813b4fc88b9fc51
wanghan <wanghan_394598004@163.com>
2020-08-06 13:20:14 +0800

    321321

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ const scene = viewer.scene;
 //开启地形深度检测
 // scene.globe.depthTestAgainstTerrain=true
 let handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+console.log("handle",handler);
 handler.setInputAction(evt => {
     // scene.globe.depthTestAgainstTerrain = true;
     let feature = scene.pick(evt.position);
@@ -79,7 +80,6 @@ handler.setInputAction(evt => {
         } else {
             popup.position = position;
         }
-        console.log(feature.id.properties);
         popup.innerHTML = `<h1>${feature.id.properties.propertyNames.map(key => {
             return `${key}:${feature.id.properties[key].getValue()}`
         }).join('\n')}</h1>`;
