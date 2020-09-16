f02becc3f19f0fc570963925e5981e98b96e9dcc
wanghan <wanghan_394598004@163.com>
2020-09-16 17:30:25 +0800

    dsadsa

--- src/Source/Overlay.js
+++ src/Source/Overlay.js
@@ -1,11 +1,11 @@
 export default class Overlay {
   constructor(option = {}) {
-    option = Object.assign({}, option);
     let _option = {
       element: null,
       position: null,
       positioning: 'top-center',
       autoPan:false,
+      offset:[0,0]
     }
     this._option = Object.assign(_option, option)
     if(this._option.viewer&&(this._option.viewer instanceof Cesium.Viewer)){
@@ -53,8 +53,10 @@ export default class Overlay {
   getElement() {
     return this._option.element
   }
+  
   _addListener() {
     this._listener = this._viewer.scene.postRender.addEventListener(() => {
+      if(!this._option.position) return;
       let windowPosition = Cesium.SceneTransforms.wgs84ToWindowCoordinates(this._viewer.scene, this._option.position);
       if (!windowPosition) return;
       this._option.element.style.left = windowPosition.x + 'px';
@@ -62,19 +64,16 @@ export default class Overlay {
     })
   }
   setPosition(position) {
-    if(!this._viewer||!this._option.element) return
+    if(!this._viewer||!this._option.element) return;
+    this._listener && this._listener();
+    this._listener=null;
     if (position) {
       this._option.position = position;
       (!this._listener) && this._addListener()
       this._option.element.style.display = 'block';
-      if(this._option.autoPan){
-
-      }
     } else {
       this._option.element.style.display = 'none';
       this._option.position = undefined;
-      this._listener && this._listener();
-      this._listener = null
     }
   }
   destory() {
